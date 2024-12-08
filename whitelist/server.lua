3534faf16de953f78b8956f0dcab512151582954
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2024-12-08 10:14:24 +0100

    Update server.lua

--- whitelist/server.lua
+++ whitelist/server.lua
@@ -1,8 +1,8 @@
 addEventHandler("onPlayerJoin", getRootElement(),
-	function()
-    	local serial = exports["whitelist"]:whiteListSerial(getPlayerSerial(source)) -- Mappából exportáljuk a létrehozott funkciót
-      	if not (serial) then
-       		  kickPlayer(source, "Fejlesztés/Karbantartás folyik a szerveren!!!!")
-        end
+    function()
+        local whiteSerial = exports["whitelist"]:getWhiteSerials(source) -- Hivatkozunk a 'global.lua' fájlban lévő funkcióra, "whitelist" helyére az általatok létrehozott mappát kell írni
+        if not (whiteSerial) then
+            kickPlayer(source, "Fejlesztés/Karbantartás folyik a szerveren!!!!")
+         end
     end
 )
