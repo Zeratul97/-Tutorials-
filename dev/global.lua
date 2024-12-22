122dd31e7f90ea492ab6cabb8d1c4046ff4b19fb
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2024-12-22 22:15:22 +0100

    Update global.lua

--- dev/global.lua
+++ dev/global.lua
@@ -1,1 +1,56 @@
+-- Fejleszők serial-jai: --
+devSerials = {
+    ["ide írd a serial-od"] = true,
+}
+-- Tiltott parancsok --
+disabledCommands = {
+    ["login"] = true,
+    ["register"] = true,
+}
 
+-- Fejlesztői parancsok --
+developerCommands = {
+    ["start"] = true,
+    ["stop"] = true,
+    ["restart"] = true,
+    ["refresh"] = true,
+    ["debugscript"] = true,
+}
+
+function isPlayerDeveloper(player)
+    if (player) then
+         if (devSerial[getPlayerSerial(player)]) then
+             return true
+         else
+             return false
+         end
+    else
+         return false
+    end
+end
+
+-- Le ellenőrizzük hogy a tilott parancsok listjában szerepel-e a parancs: --
+function isCommandDisabled(cmd)
+    if (cmd) then
+         if (disabledCommands[cmd]) then
+             return true
+         else
+             return false
+         end
+    else
+         return false
+    end
+end
+
+-- Le ellenőrizzük hogy a fejlesztői parancsok listájában szerepel-e a parancs: --
+function isDeveloperCommands(cmd)
+    if (cmd) then
+         if (developerCommands[cmd]) then
+             return true
+         else
+             return false
+         end
+    else
+         return false
+    end
+end
