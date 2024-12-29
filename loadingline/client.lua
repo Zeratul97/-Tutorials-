b485ac08fad7bb5f93b69ec60b3679095bfec8e9
Zeratul97 <133575424+Zeratul97@users.noreply.github.com>
2024-12-29 12:11:40 +0100

    Update client.lua

--- loadingline/client.lua
+++ loadingline/client.lua
@@ -1,1 +1,18 @@
+local screenX, screenY = guiGetScreenSize() -- MTA felbontás lekérdezése(kirajzolás miatt)
+local isLoadingLineShowing = true -- Látszódik-e a betöltési folyamat
+local loadingNumber = 0 -- Töltés folyamatát befolyásoló változó
 
+addEventHandler("onClientRender", getRootElement(),
+    function()
+       if (isLoadingLineShowing) then
+           dxDrawRectangle(screenX/2, screenY/2, 500, 40, tocolor(0, 0, 0, 200), false)
+           dxDrawRectangle(screenX/2+5, screenY/2+5, loadingNumber, 30, tocolor(200, 0, 0, 200), false)
+           if (loadingNumber < 490) then
+               loadingNumber = loadingNumber + 1
+           else
+               loadingNumber = 0
+               isLoadingLineShowing = false
+           end
+       end
+    end
+)
